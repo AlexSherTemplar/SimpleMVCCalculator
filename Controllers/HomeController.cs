f26e30c19b1c77f0d702505495e4c422053b56ab
tduk-tik <tduk-tik@mail.ru>
2020-06-30 14:49:21 +0300

    Калькулятор без дизайна. Только логика

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string fst, string sec, string operation)
+        {
+            int a = Convert.ToInt32(fst);
+            int b = Convert.ToInt32(sec);
+            int c = 0;
+            switch (operation)
+            {
+                case "+":
+                    c = a + b;
+                    break;
+                case "-":
+                    c = a - b;
+                    break;
+                case "*":
+                    c = a * b;
+                    break;
+                case "/":
+                    c = a / b;
+                    break;
+                default:
+                    c = 0;
+                    break;
+            }
+            ViewBag.Result = c;
+            return View();
+        }
+
+    }
+}
