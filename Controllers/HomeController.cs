98e9d91aab3d296828e2c3e6819fea29cd44f1fb
tduk-tik <tduk-tik@mail.ru>
2020-06-30 23:54:07 +0300

    Внедрил историю последних операций Подправил дизайн

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,41 +8,51 @@ namespace MVCCalculator.Controllers
 {
     public class HomeController : Controller
     {
+        public static List<HistoryItem> history = new List<HistoryItem>();
         [HttpGet]
         public ActionResult Index()
         {
+            ViewBag.history = history;
             return View();
         }
         [HttpPost]
         public ActionResult Index(string fst, string sec, string operation)
         {
-            int a, b;
+            double a, b;
             a = b = 0;
             if (!String.IsNullOrEmpty(fst) && !String.IsNullOrEmpty(sec))
             {
-                a = Convert.ToInt32(fst);
-                b = Convert.ToInt32(sec);
+                a = Convert.ToDouble(fst);
+                b = Convert.ToDouble(sec);
             }
-                int c = 0;
-                switch (operation)
-                {
-                    case "+":
-                        c = a + b;
-                        break;
-                    case "-":
-                        c = a - b;
-                        break;
-                    case "*":
-                        c = a * b;
-                        break;
-                    case "/":
-                        c = a / b;
-                        break;
-                    
-                }
-                ViewBag.Result = c;
-            
+            double c = 0;
+            switch (operation)
+            {
+                case "+":
+                    c = a + b;
+                    break;
+                case "-":
+                    c = a - b;
+                    break;
+                case "*":
+                    c = a * b;
+                    break;
+                case "/":
+                    c = a / b;
+                    break;
+
+            }
+            ViewBag.Result = c;
+            history.Add(new HistoryItem { a=a.ToString(), b=b.ToString(), oper = operation,res = c.ToString() });
+            ViewBag.history = history;
             return View();
+        }
+        public class HistoryItem
+        {
+            public string a { get; set; }
+            public string b { get; set; }
+            public string oper { get; set; }
+            public string res { get; set; }
         }
 
     }
